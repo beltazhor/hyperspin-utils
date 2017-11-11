bca1e4f56dc48528ee5707ca74d90cf9d0f3e547
beltazhor <beltazhor@gmail.com>
2017-11-11 13:17:05 +0100

    Initial commit

---
+++ hyperspin-utils/src/es/jpulgarinp/hyperspin/utils/romUtils/XMLDatabaseModifier.java
@@ -0,0 +1,17 @@
+package es.jpulgarinp.hyperspin.utils.romUtils;
+
+/**
+ * @author Jesus Pulgarin
+ * Class for modifying ROM names in hyperspin databases
+ * It takes as input the actual database and a file with the correspondency between rom code names and "understandable" names
+ * 
+ *
+ */
+public class XMLDatabaseModifier {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
